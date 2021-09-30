17bd40483d144d714a82e63badc12f30a03ab240
adenker <adenker@uni-bremen.de>
2021-09-30 15:24:23 +0200

    typos in main

--- deblurrer/main.py
+++ deblurrer/main.py
@@ -27,12 +27,12 @@ def main(input_files, output_files, step):
     experiment_name = 'step_' + str(step)  
     path_parts = [base_path, experiment_name, 'checkpoints',  'learned_gradient_descent.ckpt']
     chkp_path = os.path.join(*path_parts)
-    #reconstructor = IterativeReconstructor(radius=42, n_memory=2, n_iter=13, channels=[4,4, 8, 8, 16], skip_channels=[4,4,8,8,16])
+
     reconstructor = IterativeReconstructor.load_from_checkpoint(chkp_path)
     reconstructor.to(device)
 
     upsampling_model = UpsamplingNet(in_ch=1, hidden_ch=32, out_ch=1)
-    upsampling_model.load_state_dict(torch.load('upsampling_model.pt')) # TODO
+    upsampling_model.load_state_dict(torch.load(os.path.join(path_parts[:-1], 'upsampling_model_step_{}.pt'.format(step))))
     upsampling_model.to("cuda")
 
 
@@ -40,24 +40,23 @@ def main(input_files, output_files, step):
         if f.endswith("tif"):
             y = np.array(Image.open(os.path.join(input_files, f))) # not blurry
             print(y.shape)
-            y = torch.from_numpy(y/65535.).float()
+            y = torch.from_numpy(y).float()
+            y = (y - torch.min(y))/(torch.max(y) - torch.min(y))
             y = y.unsqueeze(0).unsqueeze(0)
             y = y.to(device)
             with torch.no_grad():
                 y = reconstructor.downsampling(y)
                 x_hat = reconstructor.forward(y)
-                x_hat = upsample(x_hat)
+                x_hat = upsampling_model(x_hat)
                 x_hat = x_hat.cpu().numpy()
 
             im = Image.fromarray(x_hat[0][0]*255.).convert("L")
-            print(im)
+            
             os.makedirs(output_files, exist_ok=True)
             im.save(os.path.join(output_files,f.split(".")[0] + ".PNG"))
 
 
     return 0
-
-
 
 
 if __name__ == "__main__":
@@ -66,45 +65,3 @@ if __name__ == "__main__":
 
 
     main(args.input_files, args.output_files, args.step)
-
-
-"""
-
-step = 19
-base_path = "/localdata/AlexanderDenker/deblurring_experiments"
-experiment_name = 'step_' + str(step)  
-version = 'version_0'
-chkp_name = 'epoch=17-step=359'
-path_parts = [base_path, 'gd_deblurring', experiment_name, 'default',
-              version, 'checkpoints', chkp_name + '.ckpt']
-chkp_path = os.path.join(*path_parts)
-#reconstructor = IterativeReconstructor(radius=42, n_memory=2, n_iter=13, channels=[4,4, 8, 8, 16], skip_channels=[4,4,8,8,16])
-reconstructor = IterativeReconstructor.load_from_checkpoint(chkp_path)
-reconstructor.to("cuda")
-
-#print("radius", reconstructor.hparams.radius)
-
-
-with torch.no_grad():
-    blur = BokehBlur(r=radius_dict[step], shape=reconstructor.hparams.img_shape)
-    #images = 1 - images
-    image_deblur = blur(images)
-    
-    image_deblur = image_deblur.to("cuda")
-    image_reco = reconstructor.forward(image_deblur)
-    image_deblur = image_deblur.cpu()
-    image_reco = image_reco.cpu()
-
-    fig, axes = plt.subplots(batch_size, 3)
-    for i in range(image_reco.shape[0]):
-
-        axes[i,0].imshow(images[i,0,:,:], cmap="gray")
-        axes[i,0].set_title("gt x")
-        axes[i,1].imshow(image_deblur[i,0,:,:], cmap="gray")
-        axes[i,1].set_title("blurred y")
-        axes[i,2].imshow(image_reco[i,0,:,:], cmap="gray")
-        axes[i,2].set_title("reconstruction")
-    plt.savefig("sanity_check_blur_" + str(step) + ".png")
-    plt.show()
-
-"""
